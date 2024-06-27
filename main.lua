986bf0d2be7823f8b5c7fe431e178c7411e1f4e5
RDW <duckwhale@mail.ru>
2024-06-27 14:14:33 +0200

    Bot: Add some placeholder slash commands

--- main.lua
+++ main.lua
@@ -1,8 +1,21 @@
+local commands = require("commands")
+
 local discordia = require("discordia")
 local client = discordia.Client()
 
 client:on("ready", function()
 	print("Logged in as ".. client.user.username)
+end)
+
+client:on('messageCreate', function(message)
+	if message.guild then
+		local serverID = message.guild.id
+		local serverCommands = commands[serverID]
+
+		if commands and serverCommands[message.content] then
+			message.channel:send(serverCommands[message.content])
+		end
+	end
 end)
 
 local tokenFile = io.open("discord.token")
